228a1110a1f5ca2a3eba6434f09423d8ed509d96
Dzénetan Massart <46716929+DzenetanMassart@users.noreply.github.com>
2019-07-17 11:06:38 +0200

    Update entity.js

--- assets/js/page_build/entity.js
+++ assets/js/page_build/entity.js
@@ -44,8 +44,8 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
             }
 
             document.getElementById("infos").innerHTML = "<p>Bienvenue dans cette quatrième version de \"Parlote\"! </p>";
-            document.getElementById("infos").innerHTML += "<p>Dzénetan Massart vous propose trois bases pour créer un personnage (Canvas & Image) qui parle en fonction de l'intensité du son dans votre microphone ! </p>";
-            document.getElementById("infos").innerHTML += "<p> Le répertoire Github associé se trouve ici: <a href=\"https://github.com/DzenetanMassart/Parlote3\">https://github.com/DzenetanMassart/Parlote4<a></p>";
+            document.getElementById("infos").innerHTML += "<p>Dzénetan Massart vous propose quatre bases pour créer un personnage (Canvas & Image) qui parle en fonction de l'intensité du son dans votre microphone ! </p>";
+            document.getElementById("infos").innerHTML += "<p> Le répertoire Github associé se trouve ici: <a href=\"https://github.com/DzenetanMassart/Parlote4\">https://github.com/DzenetanMassart/Parlote4<a></p>";
             document.getElementById("infos").innerHTML += "<p>L'intensité du son (en décibels) est égale à: </p><p class=\"average\"> " + average + "</p>";
         }
     })
